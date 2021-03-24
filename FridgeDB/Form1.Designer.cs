f0735dc35332a70737344492725b5febe5c1b441
Carol <carol@emali.com>
2021-03-24 21:56:03 +0200

    Create a Windows Form app FridgeDB

---
+++ FridgeDB/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FridgeDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listFoods = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listFoodNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Foods";
+            // 
+            // listFoods
+            // 
+            this.listFoods.FormattingEnabled = true;
+            this.listFoods.Location = new System.Drawing.Point(74, 123);
+            this.listFoods.Name = "listFoods";
+            this.listFoods.Size = new System.Drawing.Size(120, 95);
+            this.listFoods.TabIndex = 1;
+            this.listFoods.SelectedIndexChanged += new System.EventHandler(this.listFoods_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(294, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Food Names";
+            // 
+            // listFoodNames
+            // 
+            this.listFoodNames.FormattingEnabled = true;
+            this.listFoodNames.Location = new System.Drawing.Point(297, 123);
+            this.listFoodNames.Name = "listFoodNames";
+            this.listFoodNames.Size = new System.Drawing.Size(120, 95);
+            this.listFoodNames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listFoodNames);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listFoods);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listFoods;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listFoodNames;
+    }
+}
+
---
+++ FridgeDB/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FridgeDB
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["FridgeDB.Properties.Settings.FoodsConnectionString"].ConnectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateFoodsTable();
+        }
+
+        private void PopulateFoodsTable()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM FoodType", connection))
+            {
+                DataTable foodTable = new DataTable();
+                adapter.Fill(foodTable);
+
+                listFoods.DisplayMember = "TypeName";
+                listFoods.ValueMember = "Id";
+                listFoods.DataSource = foodTable;
+            }
+        }
+
+        private void listFoods_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateFoodNames();
+        }
+
+        private void PopulateFoodNames()
+        {
+            string query = "SELECT FoodInTheFridge.Name FROM FoodType INNER JOIN FoodInTheFridge ON FoodInTheFridge.TypeId = FoodType.Id WHERE FoodType.Id = @TypeId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@TypeId", listFoods.SelectedValue);
+                DataTable foodNameTable = new DataTable();
+                adapter.Fill(foodNameTable);
+
+                listFoodNames.DisplayMember = "Name";
+                listFoodNames.ValueMember = "Id";
+                listFoodNames.DataSource = foodNameTable;
+            }
+        }
+    }
+}
